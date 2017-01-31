aaad03eb0ef86a81a727f57c8d9b56a5729d467e
Nataliia Holub <golub.togo@gmail.com>
2017-01-31 19:46:27 +0200

    Done

--- function-pow.js
+++ function-pow.js
@@ -6,12 +6,24 @@
 //Создайте страницу, которая запрашивает x и n, а затем выводит результат pow(x,n).
 //P.S. В этой задаче функция обязана поддерживать только натуральные значения n, т.е. целые от 1 и выше.
 
-function powNumber( x,n ) {
-    if ( x>=1 || n >= 1){
-        return x * n;
 
+function powNumber(x, n){
+    var result = x;
+    for (var i = 1; i < n; i++){
+        result = result * x;
     }
-    alert(powNumber);
+    return result;
 }
 
-powNumber(2,3);
+var x = prompt('Введите x', '');
+var n = prompt('Введите n', '');
+
+if (n <= 1){
+    alert('Степень' + n +
+    'не поддерживается, введите целую степень, большую 1');
+}
+else {
+    alert( powNumber(x,n) );
+}
+
+
